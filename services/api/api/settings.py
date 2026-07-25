93d74123b66739bb3354571807c4dfe513fed9de
almeidava93 <88211872+almeidava93@users.noreply.github.com>
2026-07-25 20:45:15 -0300

    added log defaults

--- services/api/api/settings.py
+++ services/api/api/settings.py
@@ -1,5 +1,6 @@
 # Relevant paths
 from pathlib import Path
+from typing import Final
 
 VERSION = "0.1.0"
 TITLE = "EHR and transcript validation service"
@@ -8,3 +9,8 @@ BASE_PATH = Path(__file__).parent
 LOCAL_DATABASE_PATH = BASE_PATH.parent / "db"
 LOCAL_DATABASE_PATH.mkdir(exist_ok=True)
 TRACES_DB_PATH = LOCAL_DATABASE_PATH / "traces.db"
+
+# Logging settings
+DEFAULT_LOGGING_LEVEL = "ERROR"
+DEFAULT_LOG_FORMAT: Final = "%(asctime)s | %(levelname)-8s | %(name)s | %(message)s"
+DEFAULT_DATE_FORMAT: Final = "%Y-%m-%d %H:%M:%S"
